9308a1ac9c815088c8c1f2b79581644f3ffe5bfe
Danylo Bay <danylobay@gmail.com>
2023-06-04 23:19:08 +0200

    custom pagination

--- src/components/TweetsList/TweetsList.js
+++ src/components/TweetsList/TweetsList.js
@@ -7,6 +7,7 @@ import {
 } from 'redux/selectors';
 import './TweetsList.css';
 import { statusFilters } from 'redux/constants';
+import { useState } from 'react';
 
 export const TweetsList = () => {
   const tweets = useSelector(selectTweets);
@@ -26,13 +27,28 @@ export const TweetsList = () => {
 
   const visibleTweets = getVisibleTweets(tweets, followStatus, statusFilter);
 
+  const [numVisibleTweets, setNumVisibleTweets] = useState(3);
+
+  const handleLoadMoreClick = () => {
+    setNumVisibleTweets(numVisibleTweets + 3);
+  };
+
   return (
-    <ul className="tweets-list">
-      {visibleTweets.map(user => (
-        <li className="tweets-list_item" key={user.id}>
-          <TweetsCard tweet={user} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="tweets-list">
+        {visibleTweets.slice(0, numVisibleTweets).map(user => (
+          <li className="tweets-list_item" key={user.id}>
+            <TweetsCard tweet={user} />
+          </li>
+        ))}
+      </ul>
+      <div className="tweets-btn_item">
+        {numVisibleTweets < visibleTweets.length && (
+          <button className="tweets-button" onClick={handleLoadMoreClick}>
+            Load More
+          </button>
+        )}
+      </div>
+    </>
   );
 };
